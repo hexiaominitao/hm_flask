f5c226859fc6d973daa934df22110d52d5b12824
HeMin <a8013heming@gmail.com>
2018-01-10 11:18:08 +0800

    3rd commit

--- hello.py
+++ hello.py
@@ -1,16 +1,42 @@
-from flask import Flask
-from flask.ext.script import Manager
-app =Flask(__name__)
-manager = Manager(app)
+#! coding = utf-8
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
-@app.route('/')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField('你叫什么名字', validators=[DataRequired()])
+    submit = SubmitField('提交')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return '<h1>Hello World</h1>'
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    return render_template('index.html', form=form, name=name)
 
-@app.route('/user/<name>')
+
+@app.route('/user/name')
 def user(name):
-	return '<h1>Hello,%s!</h1> ' % name
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
-	manager.run()
+    app.run(debug=True)
